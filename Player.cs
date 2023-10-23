1ff382c85e46c370478d64c2a995c0bbde8f1f1e
Karolis Zelvys <koralbish@gmail.com>
2023-10-23 22:30:56 +0300

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
+    private void Update() {
+        Vector2 inputVector = gameInput.GetMovementNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 7f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+}
